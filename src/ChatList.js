e8c67cbd41a16654067c03f10014de9595ad82a6
Ashish Garg <mragarg@gmail.com>
2019-05-02 14:39:08 -0400

    Displays messages

--- src/ChatList.js
+++ src/ChatList.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-function ChatList() {
-  return (
-    <ul>
-      
-    </ul>
-  )
+function ChatList({messages}) {
+
+    const messageItems = messages.map((m, i) => {
+        <li key={i}>{m}</li>
+    });
+
+    return (
+        <ul>
+            {messageItems}
+        </ul>
+    )
 }
 
 export default ChatList;
