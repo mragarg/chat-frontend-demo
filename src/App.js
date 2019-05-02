4ba57dbf761277ad4b2bcb471cd84737ca277210
Ashish Garg <mragarg@gmail.com>
2019-05-02 16:01:34 -0400

    Chat fixed. Type and submit prints message

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React from 'react';
 import axios from 'axios';
 
 import ChatList from './ChatList';
+import ChatForm from './ChatForm';
+import qs from 'qs';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
   
     this.state = {
-       messages: []
+       messages: [],
+       text: ''
     }
   }
   
 
   componentDidMount() {
-
-
     setInterval( async () => {
       const {data} = await axios.get('/api')
-      console.log(data);
+      // console.log(data);
       this.setState({
         messages: data
       });
@@ -31,9 +32,32 @@ class App extends React.Component {
       <div className="App">
         <h1> Chat App </h1>
         <ChatList messages={this.state.messages}/>
+        <ChatForm text={this.state.text} handleChange={this._setText} handleSend={this._sendMessage}/>
       </div>
     );
   }
+
+  _setText = (text) => {
+    console.log("setText");
+    this.setState({
+      text
+    });
+  }
+
+  _sendMessage = () => {
+    axios({
+      method: 'post',
+      url: '/api',
+      data: qs.stringify({
+        message: this.state.text
+      }),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+    console.log("sendMessage");
+  }
+
 }
 
 export default App;
--- src/ChatForm.js
+++ src/ChatForm.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-function ChatForm(text, handleChange, handleSend) {
+function ChatForm({text, handleChange, handleSend}) {
     return (
-        <form onSubmit={handleSend}>
-            <input value={text} onChange={handleChange}/>
-            <button onClick={handleSend}>Send</button>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSend();
+            }}>
+            <input value={text} onChange={(e) => {
+                handleChange(e.target.value);
+            }}/>
+            <button>Send</button>
         </form>
     );
 }
-
 
 export default ChatForm;
